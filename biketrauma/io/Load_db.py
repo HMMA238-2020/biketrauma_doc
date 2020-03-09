b41c003e6a47279b72d0e917c5a4061ea370e93d
Charlier Benjamin <benjamin.charlier@umontpellier.fr>
2020-03-09 21:18:36 +0100

    Use the new Class

--- biketrauma/io/Load_db.py
+++ biketrauma/io/Load_db.py
@@ -6,7 +6,7 @@ from biketrauma.io import url_db, path_target
 class Load_db:
   def __init__(self, url=url_db, target_name=path_target):
     download(url, target_name, replace=False)
-
+  
   @staticmethod
   def save_as_df():
     df_bikes = pd.read_csv(path_target, na_values="", low_memory=False, converters={'data': str, 'heure': str})

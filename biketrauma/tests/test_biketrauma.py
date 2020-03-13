cd711fe0ea031b2e490591eee0b07ef176ac5f18
Charlier Benjamin <benjamin.charlier@umontpellier.fr>
2020-03-13 11:27:09 +0100

    Add tests

---
+++ biketrauma/tests/test_biketrauma.py
@@ -0,0 +1,27 @@
+import biketrauma
+from biketrauma.io import path_target
+import hashlib
+import numpy as np
+
+import  pandas as pd
+
+def md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+
+def test_dl():
+   biketrauma.Load_db()
+   m = md5(path_target)
+   assert m == "ee8c4e0e7989bc6aac7876d7501bbf4d"
+
+def test_df():
+  df = biketrauma.get_accident(biketrauma.Load_db().save_as_df(), log_scale=False)
+  assert df['21'] == 459
+
+def test_df_log():
+  df = biketrauma.get_accident(biketrauma.Load_db().save_as_df(), log_scale=True)
+  assert np.allclose(df['92'], 7.651120176)

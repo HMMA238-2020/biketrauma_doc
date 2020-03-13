4fa4935763756c47f85ebb98c148dca8f9ebf6d5
Charlier Benjamin <benjamin.charlier@umontpellier.fr>
2020-03-13 11:43:49 +0100

    Fix CI

--- biketrauma/tests/test_biketrauma.py
+++ biketrauma/tests/test_biketrauma.py
@@ -1,3 +1,9 @@
+# the following lines add the root directory of the project to os.path
+import os.path
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + (os.path.sep + '..')*2)
+
 import biketrauma
 from biketrauma.io import path_target
 import hashlib

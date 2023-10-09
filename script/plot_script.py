58915e6fe46734a3f5a6366f4b51af9325dd0fd2
Joseph Salmon <joseph.salmon@umontpellier.fr>
2023-10-09 22:34:31 +0200

    using iframe for interactive svg

--- script/plot_script.py
+++ script/plot_script.py
@@ -3,9 +3,15 @@ This is my example-script
 ==========================
 
 This example doesn't do much, it just makes a simple plot
-"""
 
+
+.. raw:: html
+
+    <iframe frameborder=0  width=100% height=600 src="../../../images/biketrauma_map.svg"></iframe>
+
+"""
 import biketrauma
+
 
 df = biketrauma.Load_db().save_as_df()
 biketrauma.plot_location(biketrauma.get_accident(df))

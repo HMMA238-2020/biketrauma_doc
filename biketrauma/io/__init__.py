9094e1398930d0c414b8915c631924eea21dd951
Joseph Salmon <joseph.salmon@umontpellier.fr>
2023-10-09 21:58:31 +0200

    updating and making svg work

--- biketrauma/io/__init__.py
+++ biketrauma/io/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-url_db = "https://koumoul.com/s/data-fair/api/v1/datasets/accidents-velos/raw"
+url_db = "https://github.com/josephsalmon/HAX712X/raw/main/Data/accidents-velos_2022.csv.xz"
 path_target = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)), "..", "data", "bicycle_db.csv"
+    os.path.dirname(os.path.realpath(__file__)), "..", "data", "bicycle_db.csv.xz"
 )
